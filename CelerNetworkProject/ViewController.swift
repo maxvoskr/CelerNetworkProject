8573a4d13be1c0a485eb6dd706ca42a49259fe42
maxvoskr <mvoskres@ucsd.edu>
2019-06-10 00:05:18 -0700

    Initial Commit

---
+++ CelerNetworkProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CelerNetworkProject
+//
+//  Created by Maksym Voskresenskyy on 6/10/19.
+//  Copyright © 2019 Maksym Voskresenskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
